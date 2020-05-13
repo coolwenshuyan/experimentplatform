88f4437c3780ce35b5971300587dafec980c776b
xiaofwnghuang <hxf08520215>
2020-05-13 22:42:56 +0800

    question_and_reply

---
+++ src/main/java/com/coolwen/experimentplatform/model/Question.java
@@ -0,0 +1,67 @@
+package com.coolwen.experimentplatform.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "t_question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,generator = "question_id")
+    @TableGenerator(name = "question_id",initialValue = 0,allocationSize = 1,table = "seq_table")
+    private int id;
+    private int sid;            //学生id
+    private String content;     //问题内容
+    private Date dic_datetime;    //记录时间
+
+
+    public Question(int sid, String content, Date dic_datetime) {
+        this.sid = sid;
+        this.content = content;
+        this.dic_datetime = dic_datetime;
+    }
+
+    public Question() { }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDic_datetime() {
+        return dic_datetime;
+    }
+
+    public void setDic_datetime(Date dic_datetime) {
+        this.dic_datetime = dic_datetime;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", sid=" + sid +
+                ", content='" + content + '\'' +
+                ", dic_datetime=" + dic_datetime +
+                '}';
+    }
+}
---
+++ src/main/java/com/coolwen/experimentplatform/service/ReplyService.java
@@ -0,0 +1,22 @@
+package com.coolwen.experimentplatform.service;
+
+import com.coolwen.experimentplatform.model.Reply;
+
+import java.util.List;
+
+public interface ReplyService {
+
+    //添加
+    void add (Reply reply);
+
+    //删
+    void delete(int id);
+
+    //通过id查
+    public List<Reply> findByreplycontent(int qid);
+
+    public Reply findById(int id);
+
+    //查所有
+    public List<Reply> getAll();
+}
