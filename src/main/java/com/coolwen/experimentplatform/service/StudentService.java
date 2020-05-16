dcc2992c393d994030deda45d8ea217acbb7632d
coolwenshuyan <122311139@qq.com>
2020-05-16 14:09:28 +0800

    Service缺失

--- src/main/java/com/coolwen/experimentplatform/service/StudentService.java
+++ src/main/java/com/coolwen/experimentplatform/service/StudentService.java
@@ -1,17 +1,16 @@
 package com.coolwen.experimentplatform.service;
 
-
 import com.coolwen.experimentplatform.model.Student;
 import org.springframework.data.domain.Page;
 
 /**
- * @author Artell
- * @version 2020/5/15 21:30
+ * @author CoolWen
+ * @version 2020-05-16 13:48
  */
-
-
 public interface StudentService {
     Page<Student> findAll(int pageNum);
+
     public Student findByUname(String uName);
+
     public Student addStudent(Student student);
 }
