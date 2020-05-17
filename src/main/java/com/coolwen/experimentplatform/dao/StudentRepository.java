212ae024049ffa9da2f876e934a0384b94752502
wxjot <467108798@qq.com>
2020-05-17 20:23:52 +0800

    Merge branch 'master' of D:\experimentplatform with conflicts.

--- src/main/java/com/coolwen/experimentplatform/dao/StudentRepository.java
+++ src/main/java/com/coolwen/experimentplatform/dao/StudentRepository.java
@@ -6,6 +6,7 @@ import com.coolwen.experimentplatform.model.DTO.StudentLastTestScoreDTO;
 import com.coolwen.experimentplatform.model.Student;
 import com.coolwen.experimentplatform.model.DTO.StudentTestScoreDTO;
 import com.coolwen.experimentplatform.model.DTO.StudentVo;
+import com.coolwen.experimentplatform.model.TreportGradeDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -20,33 +21,13 @@ import java.util.List;
 public interface StudentRepository extends BaseRepository<Student,Integer>,JpaSpecificationExecutor<Student> {
 
     Student findAllById(int id);
-    //
-    @Query("select new com.coolwen.experimentplatform.model.DTO.StudentTestScoreDTO " +
-            "(st.id, st.stuName, st.classId, expm.m_name, khms.mTestScore, khms.mTeststate) " +
+
+    @Query("select new com.coolwen.experimentplatform.model.DTO.StudentTestScoreDTO" +
+            "(st.id, st.stuName, st.classId, expm.m_name, khms.mTestScore, khms.mTeststate)" +
             "from Student st ,KaoHeModelScore khms ,ExpModel expm ,KaoheModel khm " +
             "where st.id=khms.stuId and khms.tKaohemodleId=khm.id and khm.m_id = expm.m_id ")
     public List<StudentTestScoreDTO> listStudentMTestAnswerDTO();
-
-
-    @Query("select new com.coolwen.experimentplatform.model.DTO.StudentTestScoreDTO " +
-            "(st.id, st.stuName, st.classId, expm.m_name, khms.mReportScore, khms.mReportstate) " +
-            "from Student st ,KaoHeModelScore khms ,ExpModel expm ,KaoheModel khm " +
-            "where st.id=khms.stuId and khms.tKaohemodleId=khm.id and khm.m_id = expm.m_id ")
-    public List<StudentTestScoreDTO> listStudentMReportAnswerDTO();
-
-    @Query("select new com.coolwen.experimentplatform.model.DTO.StudentLastTestScoreDTO " +
-            "(st.stuXuehao, st.stuName, clas.className,tsc.totalScore) " +
-            "from Student st left join TotalScoreCurrent tsc on st.id = tsc.stuId " +
-            "left join ClassModel clas on clas.classId = st.classId")
-    public Page<StudentLastTestScoreDTO> listStudentLastTestScoreDTO(Pageable page);
-
-    @Query("select new com.coolwen.experimentplatform.model.DTO.StudentLastTestScoreDTO " +
-            "(st.stuXuehao, st.stuName, clas.className,tsc.totalScore) " +
-            "from Student st left join TotalScoreCurrent tsc on st.id = tsc.stuId " +
-            "left join ClassModel clas on clas.classId = st.classId " +
-            "where st.classId = ?1")
-    public Page<StudentLastTestScoreDTO> listStudentLastTestScoreDTOByClassID(int classId,Pageable page);
-
+    //    and st.id=?1
 
 //    @Query("select new com.coolwen.experimentplatform.model.DTO.StuTotalScoreCurrentDTO " +
 //            "(st.stuXuehao,st.stuName,cla.className,tsc.mTotalScore,tsc.testScore,tsc.totalScore) " +
@@ -87,6 +68,6 @@ public interface StudentRepository extends BaseRepository<Student,Integer>,JpaSp
     Student findAllByStuUname(String stuUname);
 
 
-    @Query(value = "select stu_uname from t_student where id = ?",nativeQuery = true)
-    String findStudentname(int a);
+//    List<TreportGradeDto> ListStudentDto();
+
 }
