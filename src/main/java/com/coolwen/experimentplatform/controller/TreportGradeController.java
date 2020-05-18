bd3452fccdcec34c7351a82c054756f6af10333d
mu_wen <850074298@qq.com>
2020-05-18 23:39:44 +0800

    I don't want to do it anymore

--- src/main/java/com/coolwen/experimentplatform/controller/TreportGradeController.java
+++ src/main/java/com/coolwen/experimentplatform/controller/TreportGradeController.java
@@ -8,9 +8,7 @@ import com.coolwen.experimentplatform.model.*;
 import com.coolwen.experimentplatform.model.DTO.PScoreDto;
 import com.coolwen.experimentplatform.model.DTO.StudentTestScoreDTO;
 //import com.coolwen.experimentplatform.model.StudentTestScoreDTO;
-import com.coolwen.experimentplatform.service.ReportAnswerService;
-import com.coolwen.experimentplatform.service.ScoreService;
-import com.coolwen.experimentplatform.service.StudentService;
+import com.coolwen.experimentplatform.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
@@ -44,6 +42,15 @@ public class TreportGradeController {
     @Autowired
     private ReportAnswerService reportAnswerService;
 
+    @Autowired
+    private KaoHeModelScoreService kaoHeModelScoreService;
+
+    @Autowired
+    private KaoheModelService kaoheModelService;
+
+    @Autowired
+    private TotalScoreCurrentService totalScoreCurrentService;
+
 //    @GetMapping(value = "/add")
 //    public String loadAllModel(Model model) {
 //        List<StudentTestScoreDTO> page = studentRepository.listStudentMTestAnswerDTO();
@@ -67,10 +74,10 @@ public class TreportGradeController {
 
     @GetMapping(value = "/{stuId}/{mid}/giveMark" )
     public String GiveAmark(Model model,
-                            @RequestParam(required = true, defaultValue = "1") int stuId,
-                            @RequestParam(required = true, defaultValue = "1") int mid
+                            @PathVariable("stuId") int stuId,
+                            @PathVariable("mid")int mid
     ) {
-        List<PScoreDto> score = scoreService.listScorerDTOBystudentId(stuId,2);
+        List<PScoreDto> score = scoreService.listScorerDTOBystudentId(stuId,mid);
         model.addAttribute("zjy",score);
         System.out.println(">>>>>>>>>>>>>>>>>>"+score);
         return "kaohe/reportGrade_ma";
@@ -80,7 +87,7 @@ public class TreportGradeController {
 
     @PostMapping(value = "/{stuId}/{mid}/giveMark" )
     public String giveamark(Model model, HttpServletRequest request,
-                            @RequestParam(required = true, defaultValue = "1") int stuId,
+                            @PathVariable("stuId") int stuId,
                             @PathVariable("mid")int mid
 
     ) {
@@ -89,18 +96,44 @@ public class TreportGradeController {
         System.out.println(">>>>>>>>>>>>>>>>>>"+score);
         Enumeration em = request.getParameterNames();
         List<String> zyy = new ArrayList<>();
+
         while (em.hasMoreElements()) {
         String name = (String) em.nextElement();
         String value = request.getParameter(name);
         System.out.println("<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"+value);
         zyy.add(value);
         }
+
+        float fs = 0;
         for (int i = 0; i <zyy.size() ; i++) {
             PScoreDto d= score.get(i);
             ReportAnswer c = reportAnswerService.findByReportByreportid(d.getReportid());
-            c.setScore(Integer.parseInt(zyy.get(i)));
+            Integer a = Integer.parseInt(zyy.get(i));
+            fs+=a;
+            c.setScore(a);
             reportAnswerService.updateOne(c);
         }
+
+
+        KaoheModel kh = kaoheModelService.findKaoheModelByMid(mid);
+
+        KaoHeModelScore khs = kaoHeModelScoreService.findKaoheModelScoreByMid(mid ,stuId);
+        System.out.println("dddddddddddd"+khs);
+        khs.setmReportScore(fs);
+
+        float ms = (fs*kh.getM_report_baifenbi()+khs.getmTestScore()*kh.getM_test_baifenbi())*khs.getmScale();
+        khs.setmScore(ms);
+        khs.setmReportstate(true);
+
+        kaoHeModelScoreService.update(khs);
+
+        TotalScoreCurrent tsc = totalScoreCurrentService.findTotalScoreCurrentByStuID(stuId);
+
+        tsc.setmTotalScore(tsc.getmTotalScore()+ms);
+        tsc.setTotalScore(tsc.getmTotalScore()*kh.getKaohe_baifenbi()+tsc.getTestScore()*kh.getTest_baifenbi());
+
+        totalScoreCurrentService.update(tsc);
+
         return "kaohe/reportGrade_ma";
     }
 }
