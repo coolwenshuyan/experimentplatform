5a104d39b434b0f7c02520bfb694efd5c7b11f7e
zhuzhiwenandzzw <1530448242@qq.com>
2020-06-18 23:23:30 +0800

    zhuzhiwen

--- src/main/java/com/coolwen/experimentplatform/controller/SetStudentInfoController.java
+++ src/main/java/com/coolwen/experimentplatform/controller/SetStudentInfoController.java
@@ -4,15 +4,16 @@ import com.coolwen.experimentplatform.kit.ShiroKit;
 import com.coolwen.experimentplatform.model.Student;
 import com.coolwen.experimentplatform.service.SetStudentInfoService;
 import com.coolwen.experimentplatform.service.SetStudentInfoServiceImpl;
+import com.coolwen.experimentplatform.service.StudentService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.session.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Controller
 @RequestMapping(value = "/setstudentinfo")
@@ -22,6 +23,8 @@ public class SetStudentInfoController {
     SetStudentInfoService setStudentInfoService;
     @Autowired
     SetStudentInfoServiceImpl setStudentInfoServiceimpl;
+    @Autowired
+    StudentService studentService;
 
     @GetMapping(value = "/update")
     public String update(Model model) {
@@ -34,15 +37,41 @@ public class SetStudentInfoController {
     }
 
     @PostMapping(value = "/update")
-    public String update(Student student) {
-        Student student1 = (Student) SecurityUtils.getSubject().getPrincipal();
-        student.setId(student1.getId());
-        student.setStuPassword(ShiroKit.md5(student.getStuPassword(),student.getStuUname()));
-        student.setClassId(student1.getClassId());
-        student.setStuCheckstate(student1.isStuCheckstate());
-        student.setStuIsinschool(student1.isStuIsinschool());
-        student.setStuXuehao(student1.getStuXuehao());
-        setStudentInfoService.add(student);
-        return "redirect:/login";
+    public String update(Student student,Model model,@RequestParam String pass){
+        Student student3 = (Student) SecurityUtils.getSubject().getPrincipal();
+        Student student1 = studentService.findByUname(student.getStuUname());
+        if (pass.equals(student.getStuPassword())) {
+            if (student3.getStuUname().equals(student.getStuUname())){
+                System.out.println("通过");
+            }
+            else if (student1 != null) {
+                System.out.println("不通过");
+                model.addAttribute("msg1", "用户名已存在");
+                return "home_shiyan/change";
+            }
+            Pattern p = Pattern.compile("^[1](([3|5|8][\\d])|([4][4,5,6,7,8,9])|([6][2,5,6,7])|([7][^9])|([9][1,8,9]))[\\d]{8}$");
+            Matcher m = p.matcher(student.getStuMobile());
+            if (m.matches() != true){
+                model.addAttribute("telmsg", "请输入11位数字");
+                return "home_shiyan/change";
+            }
+            System.out.println("student:"+student);
+            Student student2 = new Student();
+            student2.setStuUname(student.getStuUname());
+            student2.setStuName(student.getStuName());
+            student2.setStuMobile(student.getStuMobile());
+            student2.setId(student3.getId());
+            student2.setStuPassword(ShiroKit.md5(student.getStuPassword(),student.getStuUname()));
+            student2.setClassId(student3.getClassId());
+            student2.setStuCheckstate(student3.isStuCheckstate());
+            student2.setStuIsinschool(student3.isStuIsinschool());
+            student2.setStuXuehao(student3.getStuXuehao());
+            setStudentInfoService.add(student2);
+            //退出登录
+            SecurityUtils.getSubject().logout();
+            return "redirect:/login";
+        }
+        model.addAttribute("msg3","两次输入密码不同");
+        return "home_shiyan/change";
     }
 }
